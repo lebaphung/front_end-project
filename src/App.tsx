8f6e1b62a13cac5ddce212a369d69355b9715963
Ba Phung Le <21130482@st.hcmuaf.edu.vn>
2024-05-13 16:14:19 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Project cuối kỳ môn Front End
+    </div>
+  );
+}
+
+export default App;
