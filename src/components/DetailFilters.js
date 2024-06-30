afb43edceb85363c715639a93d2e2655453d2215
Pblues <lungbaphe772003@gmail.com>
2024-06-30 17:02:56 +0700

    fix lỗi + gd

--- src/components/DetailFilters.js
+++ src/components/DetailFilters.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { priceFilter, sort } from '../redux/Action';
+import React, {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {filterProducts, priceFilter, sort} from '../redux/Action';
 import NumericInput from './NumericInput';
 import './style.css';
+import {Dropdown} from "react-bootstrap";
 
 export default function DetailFilters() {
     const [startPrice, setStartPrice] = useState('');
@@ -10,6 +11,18 @@ export default function DetailFilters() {
     const [typeSort, setTypeSort] = useState('');
     const dispatch = useDispatch();
     const [activeSort, setActiveSort] = useState('default'); // Track the active sort type
+    const [categories, setCategories] = useState([]);
+    const currentFilter = useSelector(state => state.filter)
+    useEffect(() => {
+        fetch('/jsondata/categories.json')
+            .then(response => response.json())
+            .then(data => setCategories(data))
+            .catch(error => console.error('Error fetching banner items:', error));
+    }, []);
+
+    const handleFilter = (filter) => {
+        dispatch(filterProducts(filter))
+    }
 
     const handleRangePriceFilter = () => {
         const start = startPrice !== '' ? Number(startPrice) : 0;
@@ -55,43 +68,55 @@ export default function DetailFilters() {
                 </div>
             </div>
             <div className="w-75 d-flex">
-                <div className="sort-group h-50 bg-light">
-                    <div className="sort-item">Sắp xếp theo:</div>
-                    <div className="sort-item">
-                        <button type="button"
-                                className={`btn ${activeSort === 'default' ? 'btn-success' : 'btn-outline-success'}`}
-                                onClick={(e) => handleSort("default")}
-                        >
-                            Mặc định
-                        </button>
-                    </div>
-                    <div className="sort-item">
-                        <button type="button"
-                                className={`btn ${activeSort === 'newest' ? 'btn-success' : 'btn-outline-success'}`}
-                                onClick={(e) => handleSort("newest")}
-                        >
-                            Mới nhất
-                        </button>
-                    </div>
-                    <div className="sort-item">
-                        <button type="button"
-                                className={`btn ${activeSort === 'bestselling' ? 'btn-success' : 'btn-outline-success'}`}
-                                onClick={(e) => handleSort("bestselling")}
-                        >
-                            Bán chạy
-                        </button>
-                    </div>
-                    <div className="sort-item">
-                        <select
-                            className="form-select"
-                            aria-label="Default select example"
-                            value={typeSort}
-                            onChange={(e) => handleSort(e.target.value)}
-                        >
-                            <option value="default">Giá</option>
-                            <option value="price_asc">Giá: Thấp đến cao</option>
-                            <option value="price_desc">Giá: Cao đến thấp</option>
-                        </select>
+                <div className="sort-group h-50 bg-light justify-content-between">
+                    <select className={"text-center rounded fw-bold bg-success text-white"}
+                            style={{padding: "10px", margin: "0 20px 0 10px"}}
+                            value={currentFilter}
+                            onChange={(e) => handleFilter(e.target.value)} //bắn action
+                    >
+                        <option value="ALL">Tất cả sản phẩm</option>
+                        {categories.map((c, index) => (
+                            <option value={c.id}>{c.name}</option>
+                        ))}
+                    </select>
+                    <div className="d-flex align-items-center">
+                        <div className="sort-item">Sắp xếp theo:</div>
+                        <div className="sort-item">
+                            <button type="button"
+                                    className={`btn ${activeSort === 'default' ? 'btn-success' : 'btn-outline-success'}`}
+                                    onClick={(e) => handleSort("default")}
+                            >
+                                Mặc định
+                            </button>
+                        </div>
+                        <div className="sort-item">
+                            <button type="button"
+                                    className={`btn ${activeSort === 'newest' ? 'btn-success' : 'btn-outline-success'}`}
+                                    onClick={(e) => handleSort("newest")}
+                            >
+                                Mới nhất
+                            </button>
+                        </div>
+                        <div className="sort-item">
+                            <button type="button"
+                                    className={`btn ${activeSort === 'bestselling' ? 'btn-success' : 'btn-outline-success'}`}
+                                    onClick={(e) => handleSort("bestselling")}
+                            >
+                                Bán chạy
+                            </button>
+                        </div>
+                        <div className="sort-item">
+                            <select
+                                className="form-select"
+                                aria-label="Default select example"
+                                value={typeSort}
+                                onChange={(e) => handleSort(e.target.value)}
+                            >
+                                <option value="default">Giá</option>
+                                <option value="price_asc">Giá: Thấp đến cao</option>
+                                <option value="price_desc">Giá: Cao đến thấp</option>
+                            </select>
+                        </div>
                     </div>
                 </div>
             </div>
